797cf68fcaf25d2bcd68c14a9afffddd4b389a96
ana5-30 <anaghakrishna74@gmail.com>
2022-11-16 21:15:31 -0800

    style.css

--- index.java
+++ index.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] theArgs) {
         System.out.println("Testing");
+        getHelp();
+    }
+    public static void getHelp() {
+        System.out.print("get help\nNow!!")
     }
 }
