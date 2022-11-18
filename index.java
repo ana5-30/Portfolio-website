86cdcf5f68dc030a308cf17685112329db08ef78
ana5-30 <anaghakrishna74@gmail.com>
2022-11-18 13:36:06 -0800

    Desc

--- index.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-    public static void main(String[] theArgs) {
-        System.out.println("Testing");
-        getHelp();
-    }
-    public static void getHelp() {
-        System.out.print("get help\nNow!!")
-    }
-}
